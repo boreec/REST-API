ae880140b15a08a8a3d1452e14afc861d93610c9
Cyprien Borée <boreec@tuta.io>
2023-03-17 01:04:05 +0900

    create Person class to handle a person information more concisely

---
+++ Person.py
@@ -0,0 +1,9 @@
+class Person():
+
+    def __init__(self, id: str, firstName: str, lastName: str, email: str, birthday: str):
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.birthday = birthday
+    
