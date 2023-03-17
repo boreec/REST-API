558d6ff17e318be40bda3bddaf023a754d75cba0
Cyprien Borée <boreec@tuta.io>
2023-03-18 01:30:16 +0900

    add unit_test for route '/people' with get method

--- test_routes.py
+++ test_routes.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from app import app
 from routes import *
 
@@ -8,5 +9,10 @@ class RoutesTest(unittest.TestCase):
         self.client = app.test_client()
         self.client.testing = True
 
+    def test_get_people(self):
+        result = self.client.get('/people')
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(4, len(json.loads(result.data)))
+        
 if __name__ == '__main__':
     unittest.main()
