8f4784208c1a5d624b5ba804066f941bfe2c2470
Cyprien Borée <boreec@tuta.io>
2023-03-17 23:38:03 +0900

    add unit test for Person.verify_id() throwing Exception

--- test_Person.py
+++ test_Person.py
@@ -5,7 +5,6 @@ class TestPerson(unittest.TestCase):
     @classmethod
     def setUpClass(self):        
         self.p1 = Person('bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de','John','Doe','johndoe@example.com','1997-01-01')
-        self.p2 = Person('d5356358-b39f-4c6e-9690-2c965a607702','Jane','Doe','janedoe@example.com','1991-07-28')
 
     def test_to_tuple(self):
         t = self.p1.to_tuple()
@@ -15,6 +14,11 @@ class TestPerson(unittest.TestCase):
         self.assertEqual(t[2], self.p1['lastName'])
         self.assertEqual(t[3], self.p1['email'])
         self.assertEqual(t[4], self.p1['birthday'])
-                    
+
+    def test_verify_id_raises_exception(self):
+        self.assertRaises(Exception, Person(None, "","", "","").verify_id)
+        self.assertRaises(Exception, Person("", "","", "","").verify_id)
+        self.assertRaises(Exception, Person("invalid-format", "","", "","").verify_id)
+        
 if __name__ == '__main__':
     unittest.main()
