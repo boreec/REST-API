2785e888c485813defc96b0d71daede51d748884
Cyprien Borée <boreec@tuta.io>
2023-03-18 00:25:42 +0900

    remove unnecessary setUpClass

--- test_Person.py
+++ test_Person.py
@@ -2,18 +2,16 @@ from Person import Person
 import unittest
 
 class TestPerson(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):        
-        self.p1 = Person('bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de','John','Doe','johndoe@example.com','1997-01-01')
 
     def test_to_tuple(self):
-        t = self.p1.to_tuple()
+        p1 = Person('bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de','John','Doe','johndoe@example.com','1997-01-01')
+        t = p1.to_tuple()
         self.assertTrue(isinstance(t, tuple))
-        self.assertEqual(t[0], self.p1['id'])
-        self.assertEqual(t[1], self.p1['firstName'])
-        self.assertEqual(t[2], self.p1['lastName'])
-        self.assertEqual(t[3], self.p1['email'])
-        self.assertEqual(t[4], self.p1['birthday'])
+        self.assertEqual(t[0], p1['id'])
+        self.assertEqual(t[1], p1['firstName'])
+        self.assertEqual(t[2], p1['lastName'])
+        self.assertEqual(t[3], p1['email'])
+        self.assertEqual(t[4], p1['birthday'])
 
     def test_verify_id_raises_exception(self):
         self.assertRaises(Exception, Person(None, "","", "","").verify_id)
