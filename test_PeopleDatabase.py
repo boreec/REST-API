92e20f3c56fa54d1e80a6d881118596611ae2bea
Cyprien Borée <boreec@tuta.io>
2023-03-18 00:38:32 +0900

    add unit_test for select_all_persons()

--- test_PeopleDatabase.py
+++ test_PeopleDatabase.py
@@ -1,4 +1,5 @@
 from PeopleDatabase import PeopleDatabase
+from Person import Person
 import unittest
 
 class TestPeopleDatabase(unittest.TestCase):
@@ -6,5 +7,19 @@ class TestPeopleDatabase(unittest.TestCase):
     def setUp(self):
         self.db = PeopleDatabase()
 
+    def test_select_all_persons(self):
+        rows = self.db.select_all_persons()
+        self.assertEqual(4, len(rows))
+        for person in rows :
+            self.assertTrue(isinstance(person, Person))
+            try:
+                person.verify_id()
+                person.verify_firstName()
+                person.verify_lastName()
+                person.verify_email()
+                person.verify_birthday()
+            except Exception:
+                self.fail("Data retrieved from database is wrong format!")
+        
 if __name__ == '__main__':
     unittest.main()
