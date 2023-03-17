273fa9253e33ea53947f33eae69c17ee1f34efee
Cyprien Borée <boreec@tuta.io>
2023-03-18 00:06:06 +0900

    add unit_test to verify_birthday raising exception when birthday is wrong format

--- test_Person.py
+++ test_Person.py
@@ -60,6 +60,15 @@ class TestPerson(unittest.TestCase):
             Person('','','', 'a@b.cd','').verify_email()
         except Exception as e:
             self.fail("Person.verify_email() raised exception for good name format")
-            
+
+    def test_verify_birthday_raises_exception(self):
+        self.assertRaises(Exception, Person("","","","",None).verify_birthday)
+        self.assertRaises(Exception, Person("","","","","").verify_birthday)
+        self.assertRaises(Exception, Person("","","","","March 21st 1998").verify_birthday)
+        self.assertRaises(Exception, Person("","","","","3000-01-01").verify_birthday)
+        self.assertRaises(Exception, Person("","","","","1800-01-01").verify_birthday)
+        self.assertRaises(Exception, Person("","","","","1998-13-01").verify_birthday)
+        self.assertRaises(Exception, Person("","","","","1998-02-31").verify_birthday)
+
 if __name__ == '__main__':
     unittest.main()
