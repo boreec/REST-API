9e96a8523b6b0c3cdae7e45fd18a6ca61b797fa5
Cyprien Borée <boreec@tuta.io>
2023-03-17 19:40:49 +0900

    create data before running unittest

--- test_Person.py
+++ test_Person.py
@@ -1,8 +1,10 @@
 import unittest
 
 class TestPerson(unittest.TestCase):
-    # to do
-    pass
-
+    @classmethod
+    def setUpClass(cls):        
+        self.p1 = Person('bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de','John','Doe','johndoe@example.com','1997-01-01')
+        self.p2 = Person('d5356358-b39f-4c6e-9690-2c965a607702','Jane','Doe','janedoe@example.com','1991-07-28')
+    
 if __name__ == '__main__':
     unittest.main()
