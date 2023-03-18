38e273e4fc0f775f43719976a2f1aa9198526eac
Cyprien Borée <boreec@tuta.io>
2023-03-19 00:10:58 +0900

    add unit_test for get_person_age when id is unknown

--- test_routes.py
+++ test_routes.py
@@ -23,6 +23,10 @@ class RoutesTest(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
         self.assertEqual('bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de', json.loads(result.data)['id'])
 
+    def test_get_person_age_404(self):
+        result = self.client.get('/people/unknown-id')
+        self.assertEqual(result.status_code, 404)
+
         
 if __name__ == '__main__':
     unittest.main()
