33518e418db9733692c36c570fea6d8a5792027e
Cyprien Borée <boreec@tuta.io>
2023-03-19 00:08:05 +0900

    add unit_test for get_person_by_id when id is known

--- test_routes.py
+++ test_routes.py
@@ -17,6 +17,12 @@ class RoutesTest(unittest.TestCase):
     def test_get_person_by_id_404(self):
         result = self.client.get('/people/unknown-id')
         self.assertEqual(result.status_code, 404)
+
+    def test_get_person_by_id_200(self):
+        result = self.client.get('/people/bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de')
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual('bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de', json.loads(result.data)['id'])
+
         
 if __name__ == '__main__':
     unittest.main()
