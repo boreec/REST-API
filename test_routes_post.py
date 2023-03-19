3e69980f6298c3a7b8735bdd3fa7ce093e7373ac
Cyprien Borée <boreec@tuta.io>
2023-03-19 15:34:12 +0900

    add comments to TestRoutesPOST unit tests

--- test_routes_post.py
+++ test_routes_post.py
@@ -5,7 +5,15 @@ from app import app
 from routes import *
 
 class TestRoutesPOST(unittest.TestCase):
+    """
+    A test class for the GET routes of the API.
+    """
+    
     def setUp(self):
+        """
+        Sets up the test environment by creating a Flask test 
+        client and enabling testing mode.  
+        """
         app.config['TESTING'] = True
         self.client = app.test_client()
         self.person_data = dict(
@@ -17,6 +25,10 @@ class TestRoutesPOST(unittest.TestCase):
         )
         
     def test_create_person_success(self):
+        """
+        This test checks whether a person can be successfully created by
+        sending a POST request to the '/people' endpoint with valid data.   
+        """
         result = self.client.post('/people', 
             data=json.dumps(self.person_data), 
             content_type='application/json')
@@ -26,6 +38,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(self.person_data, created_person)
 
     def test_create_person_fails_for_id_set_to_None(self):
+        """
+        This test checks whether a person creation fails if the 'id' field
+        in the person data is set to None.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['id'] = None
         result = self.client.post('/people', 
@@ -34,6 +50,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_id_set_to_empty_str(self):
+        """
+        This test checks whether a person creation fails if the 'id' field 
+        in the person data is set to an empty string.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['id'] = ""
         result = self.client.post('/people', 
@@ -42,6 +62,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_id_set_to_invalid_uuid_format(self):
+        """
+        This test checks whether a person creation fails if the 'id' field
+        in the person data is set to an invalid UUID format.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['id'] = "02830-238028-308838-dsndsds"
         result = self.client.post('/people', 
@@ -50,6 +74,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_id_already_taken(self):
+        """
+        This test checks whether a person creation fails if the 'id' field 
+        in the person data is already taken by someone else.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['id'] = "bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de"
         result = self.client.post('/people', 
@@ -58,6 +86,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_firstName_set_to_None(self):
+        """
+        This test checks whether a person creation fails if the 'firstName' field
+        in the person data is set to None.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['firstName'] = None
         result = self.client.post('/people', 
@@ -66,6 +98,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_firstName_set_to_empty_str(self):
+        """
+        This test checks whether a person creation fails if the 'firstName' field
+        in the person data is set to an empty string.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['firstName'] = ""
         result = self.client.post('/people', 
@@ -74,6 +110,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_firstName_set_to_invalid_name_format(self):
+        """
+        This test checks whether a person creation fails if the 'firstName' field 
+        in the person data is set to an invalid name format.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['firstName'] = "W@lt3rZ"
         result = self.client.post('/people', 
@@ -82,6 +122,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
         
     def test_create_person_fails_for_lastName_set_to_None(self):
+        """
+        This test checks whether a person creation fails if the 'lastName' field 
+        in the person data is set to None.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['lastName'] = None
         result = self.client.post('/people', 
@@ -90,6 +134,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_lastName_set_to_empty_str(self):
+        """
+        This test checks whether a person creation fails if the 'lastName' field 
+        in the person data is set to an empty string.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['lastName'] = ""
         result = self.client.post('/people', 
@@ -98,6 +146,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_lastName_set_to_invalid_name_format(self):
+        """
+        This test checks whether a person creation fails if the 'lastName' field 
+        in the person data is set to an invalid name format.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['lastName'] = "Sm!sth"
         result = self.client.post('/people', 
@@ -106,6 +158,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_email_set_to_None(self):
+        """
+        This test checks whether a person creation fails if the 'email' field in 
+        the person data is set to None
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['email'] = None
         result = self.client.post('/people', 
@@ -113,7 +169,11 @@ class TestRoutesPOST(unittest.TestCase):
             content_type='application/json')
         self.assertEqual(result.status_code, 400)
         
-    def test_create_person_fails_for_email_set_to_empty(self):
+    def test_create_person_fails_for_email_set_to_empty_str(self):
+        """
+        This test checks whether a person creation fails if the 'email' field 
+        in the person data is set to empty string.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['email'] = ""
         result = self.client.post('/people', 
@@ -122,6 +182,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
         
     def test_create_person_fails_for_email_set_to_invalid_email_format(self):
+        """
+        This test checks whether a person creation fails if the 'email' field in
+        the person data is set to invalid email format.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['email'] = "inval...id@em;ail@format.com"
         result = self.client.post('/people', 
@@ -130,6 +194,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_email_already_taken(self):
+        """
+        This test checks whether a person creation fails if the 'email' field 
+        in the person data is set to an email that is already taken by someone else.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['email'] = "johndoe@example.com"
         result = self.client.post('/people', 
@@ -138,6 +206,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_birthday_set_to_None(self):
+        """
+        This test checks whether a person creation fails if the 'birthday' field
+        in the person data is set to None
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['birthday'] = None
         result = self.client.post('/people', 
@@ -146,6 +218,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
 
     def test_create_person_fails_for_birthday_set_to_empty_str(self):
+        """
+        This test checks whether a person creation fails if the 'birthday' field
+        in the person data is set to an empty string.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['birthday'] = ""
         result = self.client.post('/people', 
@@ -154,6 +230,10 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
         
     def test_create_person_fails_for_birthday_set_to_future_date(self):
+        """
+        This test checks whether a person creation fails if the 'birthday' field
+        in the person data is set to a future date.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['birthday'] = "3230-01-01"
         result = self.client.post('/people', 
@@ -163,6 +243,10 @@ class TestRoutesPOST(unittest.TestCase):
 
         
     def test_create_person_fails_for_birthday_set_to_too_old_date(self):
+        """
+        This test checks whether a person creation fails if the 'birthday' field
+        in the person data is set to a date too old for a living human.
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['birthday'] = "1800-01-01"
         result = self.client.post('/people', 
@@ -171,6 +255,11 @@ class TestRoutesPOST(unittest.TestCase):
         self.assertEqual(result.status_code, 400)
         
     def test_create_person_fails_for_birthday_set_to_impossible_date(self):
+        """
+        This test checks whether a person creation fails if the 'birthday' field
+        in the person data is set to a date with a good format but that can not
+        be happen in real life (like a 29 february on a wrong year).
+        """
         invalid_person = copy.deepcopy(self.person_data)
         invalid_person['birthday'] = "1998-02-29"
         result = self.client.post('/people', 
