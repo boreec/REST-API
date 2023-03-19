11bfa6c5b6003bc888834d6617978a16d2a32fc0
Cyprien Borée <boreec@tuta.io>
2023-03-19 13:55:23 +0900

    create file for routes with UPDATE method

---
+++ test_routes_update.py
@@ -0,0 +1,12 @@
+import unittest
+import json
+from app import app
+from routes import *
+
+class TestRoutesUPDATE(unittest.TestCase):
+
+    # to do
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
