fd5742badde716c50e174b9dfd184189011f6b65
Cyprien Borée <boreec@tuta.io>
2023-03-19 14:00:34 +0900

    set up client in TestRoutesPUT

--- test_routes_put.py
+++ test_routes_put.py
@@ -4,9 +4,18 @@ from app import app
 from routes import *
 
 class TestRoutesPUT(unittest.TestCase):
-
-    # to do
-    pass
-
+    
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
+        self.person_data = dict(
+            id = "9d0e6be3-18e1-4e77-96ac-2e9260babe74",
+            firstName = "Jack",
+            lastName="Sparrow",
+            email="jacksparrow@pirates.com",
+            birthday="2000-10-23"
+        )
+        
+        
 if __name__ == "__main__":
     unittest.main()
