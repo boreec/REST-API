b6d234936a6016852fb0322127ccabf4e9a28e44
Cyprien Borée <boreec@tuta.io>
2023-03-19 14:49:10 +0900

    fix typos

--- test_routes_delete.py
+++ test_routes_delete.py
@@ -10,7 +10,7 @@ class TestRoutesDELETE(unittest.TestCase):
         self.client = app.test_client()
         self.client.testing = True
 
-    def delete_person_success(self):
+    def test_delete_person_success(self):
         temporary_person = dict(
             id="6e0f74c1-fbcd-4ea9-8faf-6b9d9fc14015",
             firstName="Jason",
@@ -20,27 +20,28 @@ class TestRoutesDELETE(unittest.TestCase):
         )
 
         # Put a temporary person inside the database.  
-        response = client.post(
+        response = self.client.post(
             '/people', 
             data=json.dumps(temporary_person),
             mimetype='application/json'
         )
-        assertEqual(response.status_code, 200)
-        assertEqual(json.loads(response.data), temporary_person)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), temporary_person)
 
         # Check that the person is inserted.
-        response = client.get('/people/' + temporary_person['id'])
-        assertEqual(response.status_code, 200)
-        assertEqual(json.loads(response.data), temporary_person)
+        response = self.client.get('/people/' + temporary_person['id'])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), temporary_person)
 
         # Delete the person.
-        response = client.delete('/people/' + temporary_person['id'])
-        assertEqual(response.status_code, 200)
-        assertEqual(json.loads(response.data), temporary_person)
+        response = self.client.delete('/people/' + temporary_person['id'])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), temporary_person)
 
         # Check the person does not exist anymore
-        response = client.get('/people/' + temporary_person['id'])
-        assertEqual(response.status_code, 404)
+        response = self.client.get('/people/' + temporary_person['id'])
+        self.assertEqual(response.status_code, 404)
+
         
 if __name__ == "__main__":
     unittest.main()
