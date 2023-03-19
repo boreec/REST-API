c45a9e04a864199ba4038707fb9466cead4c1172
Cyprien Borée <boreec@tuta.io>
2023-03-19 16:48:48 +0900

    break down unit_tests in small functions

--- tests/test_PeopleDatabase.py
+++ tests/test_PeopleDatabase.py
@@ -3,11 +3,21 @@ from Person import Person
 import unittest
 
 class TestPeopleDatabase(unittest.TestCase):
-
+    """
+    A class that with unit tests to ensure good behaviour
+    for class PeopleDatabase.
+    """
+    
     def setUp(self):
+        """
+        Create a new database attribute before running each unit test.
+        """
         self.db = PeopleDatabase()
 
     def test_select_all_persons(self):
+        """
+        Test the select_all_persons() function of PeopleDatabase.
+        """
         rows = self.db.select_all_persons()
         self.assertEqual(4, len(rows))
         for person in rows :
@@ -21,30 +31,30 @@ class TestPeopleDatabase(unittest.TestCase):
             except Exception:
                 self.fail("Data retrieved from database is wrong format!")
 
-    def test_select_person_by_id(self):
+    def test_select_person_by_id_for_unknown_id(self):
         # test None is returned for unknown id.
         self.assertEqual(None, self.db.select_person_by_id("41f7ad9c-0e21-43c9-bd8e-2678fe421232"))
 
+    def test_select_person_by_id_for_known_id(self):
         person = self.db.select_person_by_id("bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de")
-
         self.assertTrue(isinstance(person, Person))
         self.assertEqual("bf552a1c-fd73-4bd0-b64a-d3f69a9ff9de", person['id'])
 
-    def test_select_person_by_email(self):
-        # test None is returned for unknown email.
+    def test_select_person_by_emailfor_unknown_email(self):
         self.assertEqual(None, self.db.select_person_by_email("unknown@email.com"))
 
+    def test_select_person_by_email_for_known_email(self):
         person = self.db.select_person_by_email("johndoe@example.com")
-
         self.assertTrue(isinstance(person, Person))
         self.assertEqual("johndoe@example.com", person['email'])
 
-    def test_select_persons_by_name_starting_with(self):
-        # test no names are retrieved.
+    def test_select_persons_by_name_starting_with_unknown_prefix(self):
         self.assertEqual([], self.db.select_persons_by_name_starting_with("eazoieoaz"))
-        # test all names are retrieved.
+
+    def test_select_persons_by_name_sarting_with_empty_prefix(self):
         self.assertEqual(4, len(self.db.select_persons_by_name_starting_with("")))
-        # test only names starting with 'j' are retrieved.
+    
+    def test_select_persons_by_name_starting_with_j(self):
         persons = self.db.select_persons_by_name_starting_with("j")
         self.assertEqual(2, len(persons))
         self.assertTrue(persons[0]['firstName'] == 'John' or persons[0]['firstName'] == 'Jane')
